62d9034e16143896559e4105d3d24ce67c32e763
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-29 12:19:18 +0000

    style:header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,24 +6,26 @@ import Image from 'next/image'
 
 const Header:FC = () => {
   return (
-    <header className="w-full bg-white">
+    <header className="w-full bg-white py-4">
         <div className="container flex mx-auto gap-4">
-            <div className="w-full lg:w-1/2 justify-center lg:justify-end">
-                <h1 className="text-4xl">No Stress,<br />Djamo Rek!</h1>
-                <p> L&apos; appli pour mieux gérer votre argent.
-                        Ouvrez un compte gratuit maintenant en un clic.
-                </p>
-                <form action="#" className="flex gap-8">
-                    <select className="select select-bordered w-full w-max">
-                        <option selected>+221</option>
-                    </select>
-                    <input type="text" placeholder="Votre numero de telephone" className="input input-bordered" />
-                    <button className="btn btn-primary rounded-full">Rejoindre la liste d&apos;attente</button>
-                </form>
+            <div className="w-full lg:w-1/2 flex">
+                <div className="ml-auto my-auto">
+                    <h1 className="text-8xl font-extrabold">No Stress,<br />Djamo Rek!</h1>
+                    <p className='text-2xl my-6'> L&apos; appli pour mieux gérer votre argent. <br />
+                            Ouvrez un compte gratuit maintenant en un clic.
+                    </p>
+                    <form action="#" className="flex gap-2">
+                        <select className="select select-bordered w-max text-xl">
+                            <option selected>+221</option>
+                        </select>
+                        <input type="text" placeholder="Votre numero de telephone" className="input input-bordered text-xl" />
+                        <button className="btn btn-primary py-0 rounded-full text-xl">Rejoindre la liste d&apos;attente</button>
+                    </form>
+                </div>
             </div>
-            <div className="hidden lg:block w-1/2 justify-start">
+            <div className="hidden w-1/2 justify-start lg:flex">
                 <Image alt='djamo app in phone' src={DjamoAppInPhone}/>
-                <Image alt='djamo card small' src={DjamoCardSmall}/>
+                <Image alt='djamo card small' className='h-1/2 mt-auto w-1/4' src={DjamoCardSmall}/>
             </div>
         </div>
     </header>
